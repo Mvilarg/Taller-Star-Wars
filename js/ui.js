848845dde8c1638d756f39bcaff11efe5b6e4b9b
Mario Villatoro <mario.villa97@gmail.com>
2026-07-29 22:50:46 -0600

    :construction: feat: funcion JS del menu inicio y opciones

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,37 @@
 const btnHamburguesa = document.getElementById("btn-hamburguesa");
 const menuNavegacion = document.getElementById("menu-navegacion");
+const enlacesMenu = document.querySelectorAll("#menu-navegacion a");
+const imgFondo = document.getElementById("logo");
+const btnInicio = document.getElementById("btn-inicio");
 
-btnHamburguesa.addEventListener("click", function(){
+// Abrir / Cerrar menú hamburguesa
+btnHamburguesa.addEventListener("click", function() {
     menuNavegacion.classList.toggle("oculto");
+});
+
+// Evento para los enlaces del menú
+enlacesMenu.forEach(function (enlace) {
+    enlace.addEventListener("click", function () {
+        menuNavegacion.classList.add("oculto");
+        if (enlace !== btnInicio) {
+            imgFondo.style.display = "none";
+        }
+    });
+});
+
+// Evento  para el botón INICIO
+btnInicio.addEventListener("click", function (e) {
+    e.preventDefault(); 
+
+    // muestra la imagen
+    imgFondo.style.display = "block";
+    // Oculta el menú desplegable
+    menuNavegacion.classList.add("oculto");
+    // Animación hacia el inicio
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
 
 
